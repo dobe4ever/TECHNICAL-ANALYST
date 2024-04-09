daf2e0805e2a145298ac83b45476894a28ec94ae
dobe4ever <helsola8@gmail.com>
2024-04-09 21:59:13 +0700

    v0.1

--- claude.py
+++ claude.py
@@ -73,11 +73,11 @@ def is_chart(encoded_image, media_type):
 def analyze_img(encoded_image, media_type):
     if encoded_image:
         message = client.messages.create(
-            model="claude-3-haiku-20240307",
-            # model="claude-3-sonnet-20240229",
+            # model="claude-3-haiku-20240307",
+            model="claude-3-sonnet-20240229",
             # model="claude-3-opus-20240229",
             max_tokens=3000,
-            temperature=0.5,
+            temperature=0.2,
             system=analyze_img_prompt,
             messages=[
                 {
