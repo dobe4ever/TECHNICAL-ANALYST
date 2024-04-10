dc67760b387ebd4299417ed2f35eba91b41a3671
dobe4ever <helsola8@gmail.com>
2024-04-10 08:23:39 +0700

    v0.1

--- claude.py
+++ claude.py
@@ -143,9 +143,9 @@ def analyze(encoded_image, media_type):
         return is_r
 
     analysis_resp = client.messages.create(
-        model="claude-3-haiku-20240307",
+        # model="claude-3-haiku-20240307",
         # model="claude-3-sonnet-20240229",
-        # model="claude-3-opus-20240229",
+        model="claude-3-opus-20240229",
         max_tokens=3000,
         temperature=0.1,
         system=prompt_analyze,
