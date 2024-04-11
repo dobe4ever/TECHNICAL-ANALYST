afb99076c73e376d4960c71675256b86cfaae2ac
dobe4ever <helsola8@gmail.com>
2024-04-11 09:41:47 +0700

    v0.2

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ def main():
     st.markdown("## Opinionated Intelligence")
     st.markdown("### Upload any chart\n\nInclude or exclude elements like indicators, timeframes, drawings or asset name to control the information the AI can see. This forces an impartial analysis, driven purely by the technical analysis signals present in the chart.")  # noqa: E501
     # Initialize session state variables
-    if "image" not in st.session_state:
-        st.session_state.image = None
-    if "response" not in st.session_state:
-        st.session_state.response = None
+    # if "image" not in st.session_state:
+    st.session_state.image = None
+# if "response" not in st.session_state:
+    st.session_state.response = None
 
     # Image uploader
     photo = st.file_uploader("Upload image", type=["jpg", "jpeg", "png"])
